9abf41ce06c12bfd560a371ad783db9685ba8e07
osamudiamen <osas.azamegbe@gmail.com>
2020-12-31 14:12:23 +0100

    FEAT: Added python-dotenv

--- Jumga/Jumga/settings.py
+++ Jumga/Jumga/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'rij6cpg(@@(69oin*6nay^jbw*t&gt3hg^cbjt$^%$7ke2j&nf'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
