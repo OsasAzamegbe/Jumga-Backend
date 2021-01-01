b2af2165e6e98b8b0ea80e6ab6f0fb607e35edfa
osamudiamen <osas.azamegbe@gmail.com>
2021-01-01 20:03:51 +0100

    FEAT: Created dummy payment controller

--- Jumga/api/views.py
+++ Jumga/api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+
+#Test view for payments route
+def payments (request):
+    try:
+        data = request.POST
+    except:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    return Response(data={"recieved": data}, status=status.HTTP_200_OK)
