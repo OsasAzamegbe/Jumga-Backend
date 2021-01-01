34ea6856146bbd05d335bf9bdffcc80cdc87b65f
osamudiamen <osas.azamegbe@gmail.com>
2021-01-01 19:44:22 +0100

    FEAT: Created api url route

--- Jumga/api/urls.py
+++ Jumga/api/urls.py
@@ -14,10 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from api import urls as api_urls
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1', include(api_urls, namespace="api_urls"))
 ]
