39dc36a14feb3aa9871a5a0f49e5de6538fc3f04
osamudiamen <osas.azamegbe@gmail.com>
2021-01-06 12:39:49 +0100

    FEAT: Created validate_email function

---
+++ Jumga/accounts/utils.py
@@ -0,0 +1,7 @@
+import re
+
+
+def validate_email(email):
+    regex = re.compile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+
+    return regex.match(email)
