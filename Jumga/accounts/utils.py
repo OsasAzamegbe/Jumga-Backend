a4f4a1c4ae9640b85114070084b777d4f498ecc5
osamudiamen <osas.azamegbe@gmail.com>
2021-01-06 12:46:42 +0100

    FEAT: Create validate_password function

--- Jumga/accounts/utils.py
+++ Jumga/accounts/utils.py
@@ -5,3 +5,8 @@ def validate_email(email):
     regex = re.compile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 
     return regex.match(email)
+
+def validate_password(password):
+    regex = re.compile("^(?=.*[0-9])(?=.*[A-Z])(?=.*[!@#$%^&*/\\():-~`{}[\]])[a-zA-Z0-9!@#$%^&*/\\():-~`{}[\]]{8,100}$")
+
+    return regex.match(password)
