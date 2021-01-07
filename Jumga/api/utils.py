d46b552314090f7dcc8b40ca2eae700fe168f45e
osamudiamen <osas.azamegbe@gmail.com>
2021-01-07 23:46:57 +0100

    FEAT: Charge nigerian bank

--- Jumga/api/utils.py
+++ Jumga/api/utils.py
@@ -77,3 +77,28 @@ class Rave():
 
         response = requests.get(endpoint, headers=self.headers)
         return response.json()
+
+    def charge_bank_ng(self, payload: dict) -> dict:
+        endpoint = "https://api.flutterwave.com/v3/charges?type=debit_ng_account"
+
+        response = requests.post(endpoint, headers=self.headers, data=json.dumps(payload))
+        return response.json()
+
+
+# payload = {
+#    "tx_ref":"MC-1585230ew9v5050e9",
+#    "amount":"100",
+#    "account_bank":"044",
+#    "account_number":"0690000031",
+#    "currency":"NGN",
+#    "email":"user@flw.com",
+#    "phone_number":"0902620185",
+#    "fullname":"Yemi Desola"
+# }
+
+# rave = Rave(SECRET_KEY, ENCRYPTION_KEY)
+# bank_charge = rave.charge_bank_ng(payload)
+# print(bank_charge, "\n\n\n")
+
+# valid = rave.validate_charge(bank_charge["data"]["flw_ref"], "12345", "account")
+# print(valid)
