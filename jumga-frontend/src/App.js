3f86e6efd7e7d2464238a1134df62dcc778d040c
osamudiamen <osas.azamegbe@gmail.com>
2021-01-13 17:40:53 +0100

    FEAT: Set logged in user state from localstorage

--- jumga-frontend/src/App.js
+++ jumga-frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
@@ -6,12 +6,29 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Navbar from './components/Navbar';
 import Home from './pages/home/Home';
 import Signup from './pages/signup/SignUp';
-import AuthProvider from './context/AuthProvider';
+import { useAuth } from './context/AuthProvider';
 
 const App = () => {
+  const { dispatch } = useAuth();
+
+  useEffect(() =>{
+    const user = JSON.parse(localStorage.getItem("user"));
+    const merchant = JSON.parse(localStorage.getItem("merchant"));
+    const dispatchRider = JSON.parse(localStorage.getItem("dispatchRider"));
+    if (user||merchant||dispatchRider) {
+      dispatch({
+        type: "LOGIN",
+        payload: {
+          user,
+          merchant,
+          dispatchRider
+        }
+      });
+    };
+  }, [dispatch]);
+
   return (
     <div className="App">
-      <AuthProvider>
         <Router>
           <Navbar/>
           <Switch>
@@ -19,7 +36,6 @@ const App = () => {
             <Route path="/signup/" component={Signup}/>
           </Switch>
         </Router>
-      </AuthProvider>
     </div>
   );
 };
