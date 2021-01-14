e60d229675091b80b552d9ad14cd4baf2a843dd4
osamudiamen <osas.azamegbe@gmail.com>
2021-01-14 21:31:15 +0100

    BUG: Corrected property name dispatch_rider

--- jumga-frontend/src/context/AuthProvider.js
+++ jumga-frontend/src/context/AuthProvider.js
@@ -17,13 +17,13 @@ const reducer = (state, action) => {
         case "LOGIN":
             localStorage.setItem("user", JSON.stringify(action.payload.user));
             localStorage.setItem("merchant", JSON.stringify(action.payload.merchant))
-            localStorage.setItem("dispatchRider", JSON.stringify(action.payload.dispatchRider));
+            localStorage.setItem("dispatchRider", JSON.stringify(action.payload.dispatch_rider));
             return {
                 ...state,
                 isAuthenticated: true,
                 user: action.payload.user,
                 merchant: action.payload.merchant,
-                dispatchRider: action.payload.dispatchRider
+                dispatchRider: action.payload.dispatch_rider
             };
         case "LOGOUT":
             localStorage.clear();
