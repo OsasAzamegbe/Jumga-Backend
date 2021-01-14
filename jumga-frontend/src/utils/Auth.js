76c773669106cc622461ba99a5dca56e891861db
osamudiamen <osas.azamegbe@gmail.com>
2021-01-14 18:42:17 +0100

    FEAT: Switch from axios to fetch api

--- jumga-frontend/src/utils/Auth.js
+++ jumga-frontend/src/utils/Auth.js
@@ -1,31 +1,32 @@
-import axios from 'axios';
 import Cookies from 'js-cookies';
 
 //SIGNUP FUNC
 const signup = async(body, dispatch) => {
     try{
         const csrf = Cookies.getItem('csrftoken');
-        const config = {
-            "Accept": "application/json",
-            "Content-type": "application/json",
-            "X-CSRFToken": csrf
-        };
         const url = `${process.env.REACT_APP_BACKEND_API_URL}/auth/signup`
-        const response = await axios.post(url, body, config);
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Accept": "application/json",
+                "Content-type": "application/json",
+                "X-CSRFToken": csrf
+            },
+            body: JSON.stringify(body)
+        });
         const payload = await response.json();
+
         if (response.status === 201){
             dispatch({
                 type: "LOGIN",
                 payload
             })
-        } else{
-            console.log(payload)
         };
 
         return payload;
         
     } catch(err){
-        console.log(err)
+        return {status: "error", message: "Something went wrong."}
     }
 };
 
