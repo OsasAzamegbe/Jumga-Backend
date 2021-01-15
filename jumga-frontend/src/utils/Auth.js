c3b5bd94b5cc38518f912e4104c92eaf223fc3f8
osamudiamen <osas.azamegbe@gmail.com>
2021-01-15 13:08:13 +0100

    CHORE: changed from js-cookies to js-cookie

--- jumga-frontend/src/utils/Auth.js
+++ jumga-frontend/src/utils/Auth.js
@@ -1,9 +1,9 @@
-import Cookies from 'js-cookies';
+import Cookies from 'js-cookie';
 
 //SIGNUP FUNC
 const signup = async(body) => {
     try{
-        const csrf = Cookies.getItem('csrftoken');
+        const csrf = Cookies.get('csrftoken');
         const url = `${process.env.REACT_APP_BACKEND_API_URL}/auth/signup`
         const response = await fetch(url, {
             method: "POST",
@@ -15,7 +15,7 @@ const signup = async(body) => {
             body: JSON.stringify(body)
         });
         const payload = await response.json();
-        
+
         return payload;
         
     } catch(err){
